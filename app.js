30dc22cc18804ecb758b78512f6a7768158fb371
fatiperea <fatimaperea3pppp0@gmail.com>
2025-01-22 20:12:32 -0300

    declaración de variables

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. 
+// Aquí deberás desarrollar la lógica para resolver el problema.
+
+const max=10;
+
+let amigos=[];
+
+let nombre;
+
+
