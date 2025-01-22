36cf8a1f819d9cc4bfd715b74b6a55618dc8add0
fatiperea <fatimaperea3pppp0@gmail.com>
2025-01-22 20:35:43 -0300

    compruebo que el dato ingresado no sea vacio y no sea un numero

--- app.js
+++ app.js
@@ -1,17 +1,16 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. 
+// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación.
 // Aquí deberás desarrollar la lógica para resolver el problema.
 
-const max=10;
+const max = 10;
 
-let amigos=[];
+let amigos = [];
 
 let nombre;
 
-
-
 //console.log(nombre);
 
-function agregarAmigo(){
-    nombre = document.getElementById('amigo').value;
-    console.log(nombre);
+function agregarAmigo() {
+  nombre = document.getElementById("amigo").value;
+  console.log(nombre);
+  if (nombre === "" || !isNaN(nombre)) alert("No se ha ingresado un nombre!");
 }
