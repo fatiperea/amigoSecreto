9834b37854a3e80c598eacf17c4e5268cea72efc
fatiperea <fatimaperea3pppp0@gmail.com>
2025-01-24 16:30:19 -0300

    muestro el amigo sorteado

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación.
 // Aquí deberás desarrollar la lógica para resolver el problema.
 
-const max = 5;
+const totalAmigos = 5;
 
 let amigos = [];
 
@@ -10,11 +10,13 @@ let nombre;
 let i = 0;
 
 function agregarAmigo() {
-  nombre = document.getElementById("amigo").value;
+  nombre = document.getElementById("amigo").value.toUpperCase();
 
-  if (nombre === "" || !isNaN(nombre)) alert("Por favor inserte un nombre");
-  else {
-    if (amigos.length < max) {
+  if (nombre === "" || !isNaN(nombre)) {
+    alert("Por favor inserte un nombre");
+    document.getElementById("amigo").value = "";
+  } else {
+    if (amigos.length < totalAmigos) {
       amigos.push(nombre);
     } else {
       alert("Ya se ha completado la lista de amigos");
@@ -32,12 +34,32 @@ function mostrarAmigos() {
   lista.innerHTML = "";
 
   for (i = 0; i < amigos.length; i++) {
-
-    lista.innerHTML+= `<li><b>${amigos[i]}</li><br>`;
+    lista.innerHTML += `<li><b>${amigos[i]}</li><br>`;
   }
 }
 
-function sortearAmigo() {
-  if (amigos.length > 0) return Math.floor(Math.random() * amigos.length) + 1;
-}
+/*function mostrarSorteado(indice)
+{
+  let sorteado=document.getElementById('resultado');
 
+  sorteado.innerHTML = `<li><b>${amigos[indice]}</li><br>`;
+
+  sorteado.setAttribute('title','Sorteado: ')
+
+}*/
+
+function sortearAmigo() {
+  
+  if (amigos.length > 0) {
+    let indice = Math.floor(Math.random() * totalAmigos) + 1;
+
+    console.log(indice)
+
+    let sorteado = document.getElementById("resultado");
+
+    sorteado.innerHTML = `<li><b>${amigos[indice]}</li><br>`;
+
+    //sorteado.setAttribute("title", "Sorteado: ");
+
+  }else alert('La lista está vacía!');
+}
