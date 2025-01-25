bac8eada33345df72458fb8049215e829ccbe314
fatiperea <fatimaperea3pppp0@gmail.com>
2025-01-25 01:22:42 -0300

    agrego ms correcciones y controles

--- app.js
+++ app.js
@@ -9,10 +9,22 @@ let nombre;
 
 let i = 0;
 
-function cantidadAmigos(){
+let sorteoListo = false;
 
-  totalAmigos=parseInt(prompt('Agregue cantidad de amigos: '));
+//document.getElementById("btnReiniciar").setAttribute("disabled", "true");
 
+//Se establece esta funcion para evitar una lista indefinida,
+// entendiendo que para el sorteo debe haber como minimo 2 amigos
+function cantidadAmigos() {
+  let numero = parseInt(prompt("Agregue cantidad de amigos (mayor a uno): "));
+  if (numero > 1) {
+    totalAmigos = numero;
+    document.getElementById("btnAgregar").removeAttribute("disabled");
+  } else {
+    alert("No se puede jugar con menos de 2 amigos");
+    document.getElementById("btnAgregar").setAttribute("disabled", "true");
+    cantidadAmigos();
+  }
 }
 
 cantidadAmigos();
@@ -26,6 +38,7 @@ function agregarAmigo() {
   } else {
     if (amigos.length < totalAmigos) {
       amigos.push(nombre);
+      document.getElementById("btnSortear").removeAttribute("disabled");
     } else {
       alert("Ya se ha completado la lista de amigos");
       document.getElementById("btnAgregar").setAttribute("disabled", "true");
@@ -34,8 +47,10 @@ function agregarAmigo() {
 
     mostrarAmigos();
   }
+  document.getElementById("amigo").focus();
 }
 
+//No se controla si la lista esta vacia porque ya se hizo el control desde el principio
 function mostrarAmigos() {
   let lista = document.getElementById("listaAmigos");
   lista.innerHTML = "";
@@ -46,26 +61,42 @@ function mostrarAmigos() {
 }
 
 function sortearAmigo() {
-  if (amigos.length > 0) {
-    document.getElementById("btnSortear").removeAttribute("disabled");
-
+  //let sorteoListo;
+  if (amigos.includes(undefined)) {
+    document.getElementById("btnSortear").setAttribute("disabled", "true");
+    document.getElementById("btnReiniciar").setAttribute("disabled", "true");
+    alert("La lista está incompleta!");
+    
+    //document.getElementById("btnSortear").removeAttribute("disabled");
+  } else {
     let indice = Math.floor(Math.random() * amigos.length);
 
     let sorteado = document.getElementById("resultado");
 
     sorteado.innerHTML = `<li><b>Amigo secreto: ${amigos[indice]}</li><br>`;
-  } else {
-    document.getElementById("btnSortear").setAttribute("disabled", "true");
-    alert("La lista está vacía!");
-  }
+
+    
+
+    sorteoListo = true;
+    //sorteoListo = false
+  }document.getElementById("btnReiniciar").removeAttribute("disabled");
+  //return sorteoListo;
 }
 
-function reiniciar(){
+//Se agrega funcion para reiniciar el juego
+function reiniciar() {
+  //console.log(sortearAmigo());
+  //if (sortearAmigo()) {
+    //document.getElementById("btnReiniciar").removeAttribute("disabled");
+    document.getElementById("listaAmigos").innerHTML = "";
+    document.getElementById("resultado").innerHTML = "";
+    document.getElementById("btnSortear").setAttribute("disabled", "true");
 
-  document.getElementById("listaAmigos").innerHTML='';
-  document.getElementById("resultado").innerHTML='';
-  document.getElementById("btnSortear").setAttribute("disabled", "true");
+    amigos.length = 0;
 
-  cantidadAmigos();
-
+    cantidadAmigos();
+  //} else {
+    //alert("No realizo el sorteo!");
+    //document.getElementById("btnReiniciar").setAttribute("disabled", "true");
+  //}
 }
